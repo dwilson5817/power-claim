51e226210d2faea9547331af7c08392cd924a7eb
Dylan Wilson <dwilson5817@gmail.com>
2017-04-11 17:36:59 +0100

    Create basic Spigot plugin structure

---
+++ src/main/java/uk/me/dwilson/powerclaim/Main.java
@@ -0,0 +1,13 @@
+package uk.me.dwilson.powerclaim;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by dwils on 11/04/2017.
+ */
+public class Main extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("Loading PowerClaim...");
+    }
+}
